19acb42239588220bd091808a7aaf6a1f70fd148
MimonN <m.a.nedilsky@gmail.com>
2023-03-01 08:56:11 -0500

    Add Name to AspNetUsers Table

--- GoldenLinden_API/Data/ApplicationDbContext.cs
+++ GoldenLinden_API/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GoldenLinden_API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GoldenLinden_API.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
