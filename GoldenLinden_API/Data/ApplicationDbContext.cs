e820b881ba5f1671c89940555ef0a12f86782934
MimonN <m.a.nedilsky@gmail.com>
2023-03-01 11:38:49 -0500

    Initial Seed Menu Item

--- GoldenLinden_API/Data/ApplicationDbContext.cs
+++ GoldenLinden_API/Data/ApplicationDbContext.cs
@@ -10,5 +10,47 @@ namespace GoldenLinden_API.Data
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<MenuItem> MenuItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<MenuItem>().HasData(new MenuItem
+            {
+                Id = 1,
+                Name = "Ukrainian Borscht",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "https://img.ccnull.de/1025000/preview/1025542_48f1150dc44ff42996600b34cd34a3fe.jpg",
+                Price = 7.99,
+                Category = "Soup",
+                SpecialTag = ""
+            }, new MenuItem
+            {
+                Id = 2,
+                Name = "Mushroom Soup",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "https://media.30seconds.com/tip/md/Hungarian-Mushroom-Soup-Recipe-39195-c46def2dd2-1644776743.jpg",
+                Price = 8.99,
+                Category = "Soup",
+                SpecialTag = ""
+            }, new MenuItem
+            {
+                Id = 3,
+                Name = "Roasted Beef & Goat Cheese Salad",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "https://hips.hearstapps.com/hmg-prod/images/delish-beet-salad-jpg-1526059794.jpg",
+                Price = 8.99,
+                Category = "Salad",
+                SpecialTag = "Best Seller"
+            }, new MenuItem
+            {
+                Id = 4,
+                Name = "Mixed Ukrainian Grill",
+                Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                Image = "https://www.sweetandsavorybyshinee.com/wp-content/uploads/2020/07/Grilled-Pork-Kebabs-Shashlyk-4.jpg",
+                Price = 10.99,
+                Category = "Entrée",
+                SpecialTag = ""
+            });
+        }
     }
 }
--- GoldenLinden_API/Migrations/20230301163718_InitialSeedMenuItem.Designer.cs
+++ GoldenLinden_API/Migrations/20230301163718_InitialSeedMenuItem.Designer.cs
@@ -4,6 +4,7 @@ using GoldenLinden_API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GoldenLinden_API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230301163718_InitialSeedMenuItem")]
+    partial class InitialSeedMenuItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
