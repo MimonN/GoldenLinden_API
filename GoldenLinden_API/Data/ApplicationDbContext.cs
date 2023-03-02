f77fc98fa095b73c55c906a470f9f5e6c5e6e673
MimonN <m.a.nedilsky@gmail.com>
2023-03-02 15:44:52 -0500

    Cart Item and Shopping Cart Models

--- GoldenLinden_API/Data/ApplicationDbContext.cs
+++ GoldenLinden_API/Data/ApplicationDbContext.cs
@@ -10,6 +10,8 @@ namespace GoldenLinden_API.Data
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
