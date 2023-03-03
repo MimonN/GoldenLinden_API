87f9df5f7dc4f29952f4bb7a44904156a937f6c1
MimonN <m.a.nedilsky@gmail.com>
2023-03-02 22:35:37 -0500

    OrderHeader and OrderDetails Models added

--- GoldenLinden_API/Data/ApplicationDbContext.cs
+++ GoldenLinden_API/Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace GoldenLinden_API.Data
         public DbSet<MenuItem> MenuItems { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
